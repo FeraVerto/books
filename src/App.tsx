90e1223fdd5628477d511afa6c292ad09f171649
Клевакин Артем Николаевич <themightymasha@gmail.com>
2021-06-23 15:50:35 +0500

    added BrowserRouter, BookList.tsx, CreateBook.tsx, EditBook.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Route} from 'react-router-dom';
 import './App.css';
+import {BookList} from "./ui/BookList/BookList";
+import {CreateBook} from "./ui/CreateBook/CreateBook";
+import {EditBook} from "./ui/EditBook/EditBook";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const App = () => {
+    return (
+        <div className="App">
+            <header className="App-header">
+
+            </header>
+            <main>
+                <Route path="/" render={() => <BookList/>}/>
+                <Route path="/create" render={() => <CreateBook/>}/>
+                <Route path="/edit/" render={() => <EditBook/>}/>
+            </main>
+            <footer>
+
+            </footer>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/ui/EditBook/EditBook.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const EditBook = () => {
+    return (
+        <div>edit book</div>
+    )
+}
